7e0b35e34cb5918329f5b0e64fe04836b104c28f
sunilsaini87 <sunilkumarsaini6363@gmail.com>
2025-06-20 13:43:24 +0530

    update user model

---
+++ models/User.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, models, model } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser {
+  email: string;
+  password: string;
+  _id?: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+const User = models?.User || model<IUser>("User", userSchema);
+
+export default User;
